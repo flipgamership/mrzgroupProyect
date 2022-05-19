dd2f920c72fff8be1353b40da8de6c1839cc5ae1
Juan Felipe Correa Ríos <78551261+flipgamership@users.noreply.github.com>
2022-05-19 13:59:08 -0500

    update

--- src/customer/funtions/gets/gets.js
+++ src/customer/funtions/gets/gets.js
@@ -8,292 +8,288 @@ const funtions = {};
 //     res.render("notification");
 // };
 
-
 funtions.home = (req, res) => {
-    if (req.session.loggedin) {
-        res.render("home");
-    } else {
-        res.redirect('/login')
-    }
-}
+  if (req.session.loggedin) {
+    res.render("home");
+  } else {
+    res.redirect("/login");
+  }
+};
 funtions.index = (req, res) => {
-        res.render("index");
+  res.render("index");
 };
 
 funtions.logAut = (req, res) => {
-    req.session.destroy(() => {
-        res.redirect("/");
-    });
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
 };
 funtions.login = (req, res) => {
-    if(req.session.loggedin){
-        res.redirect("/home");
-    }else{
-        res.render("login");
-    }
-    
+  if (req.session.loggedin) {
+    res.redirect("/home");
+  } else {
+    res.render("login");
+  }
 };
 funtions.registerUsers = (req, res) => {
-    if(req.session.loggedin){
-        res.render('registerUser')
-    }else{
-        res.render("login");
-    }
-        };
-
+  if (req.session.loggedin) {
+    res.render("registerUser");
+  } else {
+    res.render("login");
+  }
+};
 
 funtions.registerTable = (req, res) => {
-    if(req.session.loggedin){
-        req.getConnection((error, conn) => {
-            conn.query("SELECT * FROM usuarios", (error, results) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    res.render("registerTable", {
-                        results: results,
-                        login: true,
-                        name: req.session.name,
-                        role: req.session.role,
-                    });
-                }
-            });
-        });
-    }else{
-        res.redirect('/login')
-    }
-           
-};
-
-funtions.registerUEdit = (req, res) => {
-    if (req.session.loggedin) {
-        if (req.session.role == "SuperAdmin" || req.session.role == "Administrador") {
-            const id = req.params.id;
-            req.getConnection((error, conn) => {
-                conn.query(
-                    "SELECT * FROM usuarios WHERE id = ?", [id],
-                    (error, results) => {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            res.render("editUsers", {
-                                user: results[0],
-                                login: true,
-                                name: req.session.name,
-                                role: req.session.role,
-                            });
-
-                        }
-                    }
-                );
-            });
+  if (req.session.loggedin) {
+    req.getConnection((error, conn) => {
+      conn.query("SELECT * FROM usuarios", (error, results) => {
+        if (error) {
+          console.log(error);
         } else {
-            res.render("login", {
-                login: false,
-            });
-        }
-    } else {
-        res.redirect("/login");
-    }
-};
-funtions.passwordNew = (req, res) => {
-    if (req.session.loggedin) {
-        if (req.session.role == "SuperAdmin" || req.session.role == "Administrador") {
-            const id = req.params.id;
-            req.getConnection((error, conn) => {
-                conn.query(
-                    "SELECT * FROM usuarios WHERE id = ?", [id],
-                    (error, results) => {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            res.render("newPassword", { user: results[0], role: req.session.role, });
-                        }
-                    }
-                );
-            });
-        } else {
-            res.redirect("/home");
-        }
-    } else {
-        res.redirect("/login");
-    }
-};
-funtions.BlockUser = (req, res) => {
-    if (req.session.loggedin) {
-        
-            const bloqueado = "bloqueado"
-            const id = req.params.id;
-            req.getConnection((error, conn) => {
-                conn.query(
-                    "UPDATE usuarios SET ? WHERE id = ?", [{ role: bloqueado }, id],
-                    (error, results) => {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            res.redirect("/registerTable");
-                        }
-                    }
-                );
-            });
-        
-    }else{
-        res.redirect("/login")
-    }
-};
-
-
-funtions.statusTable = (req, res) => {
-    if (req.session.loggedin) {
-        req.getConnection((error, conn) => {
-            conn.query("SELECT * FROM status", (error, results) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    res.render("statusTable", {
-                        results: results,
-                        login: true,
-                        name: req.session.name,
-                        role: req.session.role,
-                    });
-                }
-            });
-        });
-    }else{
-        res.redirect('/login')
-    }
-   
-};
-funtions.addstatus = (req, res) => {
-    if(req.session.loggedin){
-        res.render('addstatus', {
+          res.render("registerTable", {
+            results: results,
             login: true,
             name: req.session.name,
             role: req.session.role,
-        })
-    }else{
-        res.render("login");
+          });
+        }
+      });
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
+
+funtions.registerUEdit = (req, res) => {
+  if (req.session.loggedin) {
+    if (
+      req.session.role == "SuperAdmin" ||
+      req.session.role == "Administrador"
+    ) {
+      const id = req.params.id;
+      req.getConnection((error, conn) => {
+        conn.query(
+          "SELECT * FROM usuarios WHERE id = ?",
+          [id],
+          (error, results) => {
+            if (error) {
+              console.log(error);
+            } else {
+              res.render("editUsers", {
+                user: results[0],
+                login: true,
+                name: req.session.name,
+                role: req.session.role,
+              });
+            }
+          }
+        );
+      });
+    } else {
+      res.render("login", {
+        login: false,
+      });
     }
+  } else {
+    res.redirect("/login");
+  }
+};
+funtions.passwordNew = (req, res) => {
+  if (req.session.loggedin) {
+    if (
+      req.session.role == "SuperAdmin" ||
+      req.session.role == "Administrador"
+    ) {
+      const id = req.params.id;
+      req.getConnection((error, conn) => {
+        conn.query(
+          "SELECT * FROM usuarios WHERE id = ?",
+          [id],
+          (error, results) => {
+            if (error) {
+              console.log(error);
+            } else {
+              res.render("newPassword", {
+                user: results[0],
+                role: req.session.role,
+              });
+            }
+          }
+        );
+      });
+    } else {
+      res.redirect("/home");
+    }
+  } else {
+    res.redirect("/login");
+  }
+};
+funtions.BlockUser = (req, res) => {
+  if (req.session.loggedin) {
+    const bloqueado = "bloqueado";
+    const id = req.params.id;
+    req.getConnection((error, conn) => {
+      conn.query(
+        "UPDATE usuarios SET ? WHERE id = ?",
+        [{ role: bloqueado }, id],
+        (error, results) => {
+          if (error) {
+            console.log(error);
+          } else {
+            res.redirect("/registerTable");
+          }
+        }
+      );
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
+
+funtions.statusTable = (req, res) => {
+  if (req.session.loggedin) {
+    req.getConnection((error, conn) => {
+      conn.query("SELECT * FROM status", (error, results) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.render("statusTable", {
+            results: results,
+            login: true,
+            name: req.session.name,
+            role: req.session.role,
+          });
+        }
+      });
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
+funtions.addstatus = (req, res) => {
+  if (req.session.loggedin) {
+    res.render("addstatus", {
+      login: true,
+      name: req.session.name,
+      role: req.session.role,
+    });
+  } else {
+    res.render("login");
+  }
 };
 funtions.delateStatus = (req, res) => {
-    if (req.session.loggedin) {
-            const id = req.params.id;
-            req.getConnection((error, conn) => {
-                conn.query(
-                    "DELETE FROM status WHERE id = ?", [id],
-                    (error, results) => {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            res.redirect("/statusTable");
-                        }
-                    }
-                );
-            });
-        
-    }else{
-        res.redirect("/login")
-    }
+  if (req.session.loggedin) {
+    const id = req.params.id;
+    req.getConnection((error, conn) => {
+      conn.query("DELETE FROM status WHERE id = ?", [id], (error, results) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.redirect("/statusTable");
+        }
+      });
+    });
+  } else {
+    res.redirect("/login");
+  }
 };
 funtions.addMercancia = (req, res) => {
-    if(req.session.loggedin){
-
-
-        req.getConnection((error,conn) => {
-            conn.query(
-                'SELECT * FROM STATUS ', (error, results) => {
-                    if (error) {
-                        console.log(error)
-                    }else {
-                        res.render('addMercancia', {
-                            login: true,
-                            result: results,
-                            name: req.session.name,
-                            role: req.session.role,
-                        })
-                    }
-                }
-            )
-        })
-
-        
-    }else{
-        res.render("login");
-    }
+  if (req.session.loggedin) {
+    req.getConnection((error, conn) => {
+      conn.query("SELECT * FROM STATUS ", (error, results) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.render("addMercancia", {
+            login: true,
+            result: results,
+            name: req.session.name,
+            role: req.session.role,
+          });
+        }
+      });
+    });
+  } else {
+    res.render("login");
+  }
 };
 
 funtions.mercanciaTable = (req, res) => {
-    if (req.session.loggedin) {
-        req.getConnection((error, conn) => {
-            conn.query("SELECT * FROM objetos", (error, results) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    res.render("mercanciaTable", {
-                        results: results,
-                        login: true,
-                        name: req.session.name,
-                        role: req.session.role,
-                    });
-                }
-            });
-        });
-    }else{
-        res.redirect('/login')
-    }
-   
+  if (req.session.loggedin) {
+    req.getConnection((error, conn) => {
+      conn.query("SELECT * FROM objetos", (error, results) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.render("mercanciaTable", {
+            results: results,
+            login: true,
+            name: req.session.name,
+            role: req.session.role,
+          });
+        }
+      });
+    });
+  } else {
+    res.redirect("/login");
+  }
 };
-
-
 
 funtions.mercanciaUpdateHistorial = (req, res) => {
-    if (req.session.loggedin) {
-        const id = req.params.id;
-        req.getConnection((error, conn) => {
-            conn.query("SELECT * FROM status", (error, data)=>{
-                if(error){
-                    console.log(error);
-                }else{
+  if (req.session.loggedin) {
+    const id = req.params.id;
+    req.getConnection((error, conn) => {
+      conn.query("SELECT * FROM status", (error, data) => {
+        if (error) {
+          console.log(error);
+        } else {
+          req.getConnection((error, conn) => {
+            conn.query(
+              "SELECT * FROM historial WHERE awbID = ?",
+              [id],
+              (error, resultsHistory) => {
+                if (error) {
+                  console.log(error);
+                } else {
+                  if (resultsHistory.length > 0) {
+                    res.render("addMercanciaUpdateStatus", {
+                      data: data,
+                      results: resultsHistory,
+                      login: true,
+                      name: req.session.name,
+                      role: req.session.role,
+                    });
+                  } else {
                     req.getConnection((error, conn) => {
-                        conn.query("SELECT * FROM historial WHERE awbID  = ?", [id], (error, resultsHistory)=>{
-                            if (error) {
-                                console.log(error);
-                            }else{
-                                if(resultsHistory.length > 0){
-                                  res.render('addMercanciaUpdateStatus',{
-                                      results: resultsHistory,
-                                      login: true,
-                                      name: req.session.name,
-                                      role: req.session.role,
-                                  })
-                                }else{
-                                  req.getConnection((error, conn)=>{
-                                      conn.query("SELECT * FROM objetos WHERE id = ?" [id], (error, results)=> {
-                                              if (error) {
-                                                  console.log(error);
-                                              }else{
-                                                  res.render("addMercanciaUpdateStatus",{
-                                                      results: results,
-                                                      login: true,
-                                                      name: req.session.name,
-                                                      role: req.session.role,
-                                                  })
-                                              }
-                                          }
-                                      )
-                                  })
-                                }
-                            }
-                        } )
-                    })
+                      conn.query(
+                        "SELECT * FROM objetos WHERE id = ?",
+                        [id],
+                        (error, results) => {
+                          if (error) {
+                            console.log(error);
+                          } else {
+                            res.render("addMercanciaUpdateStatus", {
+                              data: data,
+                              results: results,
+                              login: true,
+                              name: req.session.name,
+                              role: req.session.role,
+                            });
+                          }
+                        }
+                      );
+                    });
+                  }
                 }
-            })
-        })
-    }else{
-        res.redirect('/login')
-    }
-   
+              }
+            );
+          });
+        }
+      });
+    });
+  } else {
+    res.redirect("/login");
+  }
 };
-
-
 
 module.exports = funtions;
