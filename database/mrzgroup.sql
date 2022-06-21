c77b874f5f6674314c0cb2c366fe811898fcf335
Juan Felipe Correa Ríos <78551261+flipgamership@users.noreply.github.com>
2022-06-21 14:36:26 -0500

    update

--- database/mrzgroup.sql
+++ database/mrzgroup.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-06-2022 a las 14:18:32
+-- Tiempo de generación: 21-06-2022 a las 21:35:58
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -26,9 +26,33 @@ USE `mrzgroup`;
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(500) NOT NULL,
+  `correo` varchar(500) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `telefono` varchar(500) NOT NULL,
+  `role` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nombre`, `correo`, `password`, `telefono`, `role`) VALUES
+(1, 'divicions', 'thefelipegamer7@gmail.com', '$2a$08$dW0cqp5CZZ3QqRBuSqWJcuwXaxmMRvJKAqei8Zs3XQmwt1ezwB5wu', '3003044512', 'cliente');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `historial`
 --
 
+DROP TABLE IF EXISTS `historial`;
 CREATE TABLE `historial` (
   `id` int(11) NOT NULL,
   `awbID` int(11) NOT NULL,
@@ -37,12 +61,21 @@ CREATE TABLE `historial` (
   `observaciones` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `historial`
+--
+
+INSERT INTO `historial` (`id`, `awbID`, `estatusHistorial`, `fecha`, `observaciones`) VALUES
+(1, 1, 'envio', '2022-05-19', 'popayan'),
+(2, 1, 'entrega', '2022-05-19', 'se entrego');
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `imagenes`
 --
 
+DROP TABLE IF EXISTS `imagenes`;
 CREATE TABLE `imagenes` (
   `id` int(11) NOT NULL,
   `idHistorial` int(11) NOT NULL,
@@ -55,8 +88,7 @@ CREATE TABLE `imagenes` (
 --
 
 INSERT INTO `imagenes` (`id`, `idHistorial`, `statusHistorial`, `imagen`) VALUES
-(2, 1, 'envio', 'ed742179-64da-4b17-9522-d692d34b77ce.jpg'),
-(3, 1, 'envio', '6a0b9b70-bfa2-4ee6-bcaa-2072a7ce8b43.png');
+(2, 1, 'envio', 'ed742179-64da-4b17-9522-d692d34b77ce.jpg');
 
 -- --------------------------------------------------------
 
@@ -64,6 +96,7 @@ INSERT INTO `imagenes` (`id`, `idHistorial`, `statusHistorial`, `imagen`) VALUES
 -- Estructura de tabla para la tabla `objetos`
 --
 
+DROP TABLE IF EXISTS `objetos`;
 CREATE TABLE `objetos` (
   `id` int(11) NOT NULL,
   `awb` varchar(500) NOT NULL,
@@ -78,7 +111,7 @@ CREATE TABLE `objetos` (
 --
 
 INSERT INTO `objetos` (`id`, `awb`, `status`, `name`, `cliente`, `fecha`) VALUES
-(1, 'HAF1926312Ag', 'envio', 'paqueteria express', 'mercanciaSas', '2022-05-19');
+(1, 'HAF1926312Ag', 'entrega', 'paqueteria express', 'mercanciaSas', '2022-05-19');
 
 -- --------------------------------------------------------
 
@@ -86,6 +119,7 @@ INSERT INTO `objetos` (`id`, `awb`, `status`, `name`, `cliente`, `fecha`) VALUES
 -- Estructura de tabla para la tabla `servicios`
 --
 
+DROP TABLE IF EXISTS `servicios`;
 CREATE TABLE `servicios` (
   `id` int(11) NOT NULL,
   `servicio` varchar(500) NOT NULL
@@ -97,6 +131,7 @@ CREATE TABLE `servicios` (
 -- Estructura de tabla para la tabla `status`
 --
 
+DROP TABLE IF EXISTS `status`;
 CREATE TABLE `status` (
   `id` int(11) NOT NULL,
   `name` varchar(500) NOT NULL,
@@ -109,7 +144,8 @@ CREATE TABLE `status` (
 --
 
 INSERT INTO `status` (`id`, `name`, `descripcion`, `porcentaje`) VALUES
-(4, 'envio', 'el envio ', '25');
+(4, 'envio', 'el envio ', '25'),
+(5, 'entrega', 'Se entregó el paquete al domicilio solicitado', '100');
 
 -- --------------------------------------------------------
 
@@ -117,6 +153,7 @@ INSERT INTO `status` (`id`, `name`, `descripcion`, `porcentaje`) VALUES
 -- Estructura de tabla para la tabla `statusservice`
 --
 
+DROP TABLE IF EXISTS `statusservice`;
 CREATE TABLE `statusservice` (
   `id` int(11) NOT NULL,
   `name` varchar(500) NOT NULL,
@@ -130,6 +167,7 @@ CREATE TABLE `statusservice` (
 -- Estructura de tabla para la tabla `usuarios`
 --
 
+DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nombre` varchar(500) NOT NULL,
@@ -146,11 +184,18 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `password`, `correo`, `warrant`, `telefono`, `role`, `pin`) VALUES
-(1, 'dev juan felipe correa', '$2a$08$dW0cqp5CZZ3QqRBuSqWJcuwXaxmMRvJKAqei8Zs3XQmwt1ezwB5wu', 'thefelipegamer7@gmail.com', '1001724660', '3003044512', 'SuperAdmin', '');
+(1, 'dev juan felipe correa', '$2a$08$dW0cqp5CZZ3QqRBuSqWJcuwXaxmMRvJKAqei8Zs3XQmwt1ezwB5wu', 'thefelipegamer7@gmail.com', '1001724660', '3003044512', 'SuperAdmin', ''),
+(4, 'david', '$2a$08$E12YqcG.diriMS1HfNSuu.zETxIs9.IMT97l7Q3svLjDE2c2Q9oI2', 'flipgamer1313@gmail.com', '1001724660', '3003044512', 'bloqueado', '');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `historial`
@@ -195,10 +240,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `historial`
 --
 ALTER TABLE `historial`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `imagenes`
@@ -222,13 +273,13 @@ ALTER TABLE `servicios`
 -- AUTO_INCREMENT de la tabla `status`
 --
 ALTER TABLE `status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
